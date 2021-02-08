50214fd7536109d9a16d034b8767aa1b7d1a4fe1
ivanovvaleriy <varera7115@gmail.com>
2021-02-08 16:51:39 +0200

    Update 2_functional-tests.js

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -57,9 +57,12 @@ suite("Functional Tests with Zombie.js", function () {
 
   suite('"Famous Italian Explorers" form', function () {
     // #5
-    test('submit "surname" : "Colombo" - write your e2e test...', function (done) {
+     test('submit "surname" : "Colombo" - write your e2e test...', function (done) {
       browser.fill("surname", "Colombo").pressButton("submit", function () {
-        assert.fail();
+       browser.assert.success();
+    browser.assert.text('span#name', 'Cristoforo');
+    browser.assert.text('span#surname', 'Colombo');
+    browser.assert.element('span#dates', 1);
 
         done();
       });
